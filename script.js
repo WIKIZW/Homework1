597e8e3ce4e0c33fcee930b491b27c86784cbafd
WIKIZW <sasha.bulanov@inbox.lv>
2020-04-17 19:55:31 +0300

    add cycles

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 'use strict'
 
-var money,
-    time;
-money = prompt("Ваш бюджет на месяц?");
-time = prompt("Введите дату в формате YYYY-MM-DD");
+var money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 
 var appData = {
     budget: money,
     timeData: time,
-    expenses : {question1 : "question12"},
+    expenses : {},
     optionalExpenses : {},
     income : [],
     savings: false
 };
 
-let question1 = prompt("Введите обязательную статью расходов в этом месяце");
-let question2 = prompt("Во сколько обойдется?");
-let question12 = prompt("Введите обязательную статью расходов в этом месяце");
-let question22 = prompt("Во сколько обойдется?");
+for (let i = 0; i < 2; i++) {
+    let question1 = prompt("Введите обязательную статью расходов в этом месяце"),
+        question2 = prompt("Во сколько обойдется?");
 
-appData.expenses[question1] = question2;
-appData.expenses[question12] = question22;
+        appData.expenses[question1] = question2;
+}
 
 alert(appData.budget/ 30);
-console.log(appData);
